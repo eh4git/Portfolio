c9c857c5fbb756f725e2c0bead89d58b9c0eda96
erik hirsch <ehirsch760@gmail.com>
2021-03-29 12:45:41 -0700

    override MuiDrawer css

--- components/sideDrawer/index.js
+++ components/sideDrawer/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx'
 import {
-    Drawer,
-    List, 
-    ListItem, 
-    ListItemIcon, 
-    ListItemText, 
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import HomeIcon from '@material-ui/icons/Home';
+import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
 import MailIcon from '@material-ui/icons/Mail';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles, useTheme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-const drawerWidth = 240;
+const theme = createMuiTheme({
+  overrides: {
+    // Style sheet name
+    MuiDrawer: {
+      // Name of the rule
+      paper: {
+        background: "none",
+      },
+      paperAnchorDockedLeft: {
+        borderRight: "none"
+      }
+    }
+  }
+});
+
+const drawerWidth = 150;
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
     whiteSpace: 'nowrap',
+  },
+  list: {
+
   },
   drawerOpen: {
     width: drawerWidth,
@@ -27,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
     overflow: 'hidden',
-    height: '500px'
+    justifyContent: "center"
   },
   drawerClose: {
     transition: theme.transitions.create('width', {
@@ -39,59 +59,63 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('sm')]: {
       width: theme.spacing(9) + 1,
     },
+    justifyContent: "center"
   },
 }));
 
 export default function SideDrawer() {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleDrawer = () => {
     setOpen(!open);
   };
 
   return (
-      <Drawer
-        onMouseEnter={handleDrawer}
-        onMouseLeave={handleDrawer}
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+    <div style={{display:"flex", justifyContent: "center"}}>
+      <ThemeProvider theme={theme} >
+        <Drawer
+          onMouseEnter={handleDrawer}
+          onMouseLeave={handleDrawer}
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <List>
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <List className={classes.list}>
             <ListItem button>
-              <ListItemIcon><MailIcon /></ListItemIcon>
+              <ListItemIcon><HomeIcon /></ListItemIcon>
               <ListItemText primary={"Home"} />
             </ListItem>
             <ListItem button>
-              <ListItemIcon><MailIcon /></ListItemIcon>
+              <ListItemIcon><EmojiPeopleIcon /></ListItemIcon>
               <ListItemText primary={"About"} />
             </ListItem>
             <ListItem button>
-              <ListItemIcon><MailIcon /></ListItemIcon>
+              <ListItemIcon><ImportantDevicesIcon /></ListItemIcon>
               <ListItemText primary={"Projects"} />
             </ListItem>
             <ListItem button>
               <ListItemIcon><MailIcon /></ListItemIcon>
               <ListItemText primary={"Contact"} />
             </ListItem>
-        </List>
-      </Drawer>
+          </List>
+        </Drawer>
+      </ThemeProvider >
+  </div>
   );
 }
 SideDrawer.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
