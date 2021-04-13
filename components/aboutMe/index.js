7fd24c14ce9bbaf0c9545b06a10cf11fd4090021
erik hirsch <ehirsch760@gmail.com>
2021-04-12 22:24:36 -0700

    basic medium screen size layout

--- components/aboutMe/index.js
+++ components/aboutMe/index.js
@@ -4,9 +4,9 @@ import { Typography } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     section: {
-        position: "relative",
+        // position: "relative",
         display: "flex",
         flexWrap: "wrap",
         backgroundColor: "lightgray"
@@ -39,23 +39,39 @@ const useStyles = makeStyles((theme) => ({
     },
     img1: {
         position: 'relative',
-        // top: 100,
+        top: "5%",
         left: "2.5%",
         zIndex: 3,
     },
     img2: {
         position: 'relative',
-        top: "-20%",
-        left: "45%",
+        top: "-10%",
+        left: "40%",
         zIndex: 2,
     },
     img3: {
         position: 'relative',
-        top: "-25%",
+        top: "-20%",
         left: "1%",
         zIndex: 1,
-    }
-}));
+    },
+    //------ Medium Size Specific Classes --------
+    paragraphContainer: {
+        display: "flex",
+        justifyContent: "space-around",
+        marginLeft: "10%",
+        marginRight: "5%"
+    },
+    imgMd: {
+        flex: 1,
+        margin: "2.5%",
+    },
+    textMd: {
+        fontSize: "18px",
+        flex: 3,
+        margin: "2.5%",
+    },
+});
 
 const AboutMe = () => {
 
@@ -93,76 +109,147 @@ const AboutMe = () => {
         }
     }, [])
 
-
     return (
         <>
-            <span>{`(min-width:600px) matches: ${small}`}</span>
             {small && <span>Small</span>}
-            {medium && <span>Medium</span>}
-            {large && 
-            <section className={classes.section}
-                style={{ flexDirection: medium || small && "column" }}
-            >
-                <div className={classes.aboutMeContainer} >
+            {medium &&
+                <section className={classes.section}  >
+
                     <Typography className={classes.mainHeader}>
                         About
-                </Typography>
-                    <Typography className={classes.text}>
-                        Hey everyone! My name is Erik Hirsch, and I am an accredited and certified full stack web developer who possesses the necessary programming and front/back end expertise that guarantees high functioning, aesthetically pleasing results. As a founding member of Third Project Development, I have a rich background within the web and app (Android and IOS) development sphere and enjoy leveraging those collective skills to give my clients the second to none service and outcomes they deserve.
-                </Typography>
-                    <Typography className={classes.text}>
-                        Backing up a bit, I was born and raised in Southern California and have always admired the concept of developing. Being able to intertwine my creativity and problem-solving abilities with my passion for learning and skill development is what unanimously inspired me to choose web development as a career. This, along with my commitment, resilience, and strong work ethic that was instilled in me at a young age, inevitably became the catalyst for me to earn my certificate in full stack web development from UCLA Extension in July of 2020. Furthermore, I also hold a certificate in business management from Victor Valley College and developed in-depth knowledge on how to fabricate responsive websites and mobile applications, all of which has positioned me to become the respected web developing asset I am today at Third Project Development.
-                </Typography>
-                    <Typography className={classes.text}>
-                        As a professional with a notable "go-getter" attitude, helping people obtain their web/app development goals is the fundamental driver behind my passion for what I do. I built my services on a foundation of authenticity, quality, and trust, and every effort I perform is aimed to raise the industry standards bar as a whole. Overall, I have a genuine ardency for team collaboration and hold a robust inventory of accomplishments to date, all of which can give you the peace of mind that your full stack development objectives are in the best possible hands to succeed.
-                </Typography>
-                    <Typography className={classes.text}>
-                        There is no denying that I love my career, and I take pride in delivering top-quality, fully tailored services that support my client in reaching new levels of digital success. However, in my free time, you can find me enjoying the outdoors, animals, avidly hiking, and being a proud husband and father. In fact, my family is my biggest inspiration and motivation that pushes me to be the best that I can be.
-                </Typography>
-                </div>
-                <div className={classes.collageContainer}>
-                    <div className={classes.img1}>
-                        <Image
-                            src='/img/profilePicture.jpg'
-                            height={screenHeight * .3}
-                            width={screenWidth * .15}
-                            layout='intrinsic'
-                            // layout='responsive'
-                            objectFit='contain'
-                            // objectPosition='100% 80%'
-                            alt='Personal Photo'
-
-                        />
+                    </Typography>
+                    <div className={classes.paragraphContainer}>
+                        <Typography className={classes.textMd}>
+                            Hey everyone! My name is Erik Hirsch, and I am an accredited and certified full stack web developer who possesses the necessary programming and front/back end expertise that guarantees high functioning, aesthetically pleasing results. As a founding member of Third Project Development, I have a rich background within the web and app (Android and IOS) development sphere and enjoy leveraging those collective skills to give my clients the second to none service and outcomes they deserve.
+                        </Typography>
+                        <div className={classes.imgMd}>
+                            <Image
+                                src='/img/profilePicture.jpg'
+                                height={400}
+                                width={500}
+                                layout='intrinsic'
+                                // layout='responsive'
+                                objectFit='contain'
+                                objectPosition='100% 80%'
+                                alt='Personal Photo'
+                            />
+                        </div>
                     </div>
-                    <div className={classes.img2}>
-                        <Image
-                            src='/img/profilePicture.jpg'
-                            height={screenHeight * .25}
-                            width={screenWidth * .125}
-                            // layout='intrinsic'
-                            layout='intrinsic'
-                            objectFit='contain'
-                            // objectPosition='40% 100%'
-                            alt='Personal Photo'
-
-                        />
+                    <div className={classes.paragraphContainer}>
+                        <div className={classes.imgMd}>
+                            <Image
+                                src='/img/profilePicture.jpg'
+                                height={240}
+                                width={screenWidth * .15}
+                                layout='intrinsic'
+                                // layout='responsive'
+                                objectFit='contain'
+                                objectPosition='100% 80%'
+                                alt='Personal Photo'
+                            />
+                        </div>
+                        <Typography className={classes.textMd}>
+                            Backing up a bit, I was born and raised in Southern California and have always admired the concept of developing. Being able to intertwine my creativity and problem-solving abilities with my passion for learning and skill development is what unanimously inspired me to choose web development as a career. This, along with my commitment, resilience, and strong work ethic that was instilled in me at a young age, inevitably became the catalyst for me to earn my certificate in full stack web development from UCLA Extension in July of 2020. Furthermore, I also hold a certificate in business management from Victor Valley College and developed in-depth knowledge on how to fabricate responsive websites and mobile applications, all of which has positioned me to become the respected web developing asset I am today at Third Project Development.
+                        </Typography>
                     </div>
-                    <div className={classes.img3}>
-                        <Image
-                            src='/img/12721.jpg'
-                            height={screenHeight * .3}
-                            width={screenWidth * .25}
-                            layout='intrinsic'
-                            // layout='responsive'
-                            objectFit='contain'
-                            // objectPosition='100% 80%'
-                            alt='Personal Photo'
-
-                        />
+                    <div className={classes.paragraphContainer}>
+                        <Typography className={classes.textMd}>
+                            As a professional with a notable "go-getter" attitude, helping people obtain their web/app development goals is the fundamental driver behind my passion for what I do. I built my services on a foundation of authenticity, quality, and trust, and every effort I perform is aimed to raise the industry standards bar as a whole. Overall, I have a genuine ardency for team collaboration and hold a robust inventory of accomplishments to date, all of which can give you the peace of mind that your full stack development objectives are in the best possible hands to succeed.
+                        </Typography>
+                        <div className={classes.imgMd}>
+                            <Image
+                                src='/img/profilePicture.jpg'
+                                height={220}
+                                width={screenWidth * .125}
+                                // layout='intrinsic'
+                                layout='intrinsic'
+                                objectFit='contain'
+                                // objectPosition='40% 100%'
+                                alt='Personal Photo'
+                            />
+                        </div>
                     </div>
-                </div>
-            </section>}
+                    <div className={classes.paragraphContainer}>
+                        <div className={classes.imgMd}>
+                            <Image
+                                src='/img/12721.jpg'
+                                height={600}
+                                width={500}
+                                layout='intrinsic'
+                                // layout='responsive'
+                                objectFit='contain'
+                                // objectPosition='100% 80%'
+                                alt='Personal Photo'
+                            />
+                        </div>
+                        <Typography className={classes.textMd}>
+                            There is no denying that I love my career, and I take pride in delivering top-quality, fully tailored services that support my client in reaching new levels of digital success. However, in my free time, you can find me enjoying the outdoors, animals, avidly hiking, and being a proud husband and father. In fact, my family is my biggest inspiration and motivation that pushes me to be the best that I can be.
+                        </Typography>
+                    </div>
+                </section>
+            }
 
+            {large &&
+                <section className={classes.section} >
+                    <div className={classes.aboutMeContainer} >
+                        <Typography className={classes.mainHeader}>
+                            About
+                        </Typography>
+                        <Typography className={classes.text}>
+                            Hey everyone! My name is Erik Hirsch, and I am an accredited and certified full stack web developer who possesses the necessary programming and front/back end expertise that guarantees high functioning, aesthetically pleasing results. As a founding member of Third Project Development, I have a rich background within the web and app (Android and IOS) development sphere and enjoy leveraging those collective skills to give my clients the second to none service and outcomes they deserve.
+                        </Typography>
+                        <Typography className={classes.text}>
+                            Backing up a bit, I was born and raised in Southern California and have always admired the concept of developing. Being able to intertwine my creativity and problem-solving abilities with my passion for learning and skill development is what unanimously inspired me to choose web development as a career. This, along with my commitment, resilience, and strong work ethic that was instilled in me at a young age, inevitably became the catalyst for me to earn my certificate in full stack web development from UCLA Extension in July of 2020. Furthermore, I also hold a certificate in business management from Victor Valley College and developed in-depth knowledge on how to fabricate responsive websites and mobile applications, all of which has positioned me to become the respected web developing asset I am today at Third Project Development.
+                        </Typography>
+                        <Typography className={classes.text}>
+                            As a professional with a notable "go-getter" attitude, helping people obtain their web/app development goals is the fundamental driver behind my passion for what I do. I built my services on a foundation of authenticity, quality, and trust, and every effort I perform is aimed to raise the industry standards bar as a whole. Overall, I have a genuine ardency for team collaboration and hold a robust inventory of accomplishments to date, all of which can give you the peace of mind that your full stack development objectives are in the best possible hands to succeed.
+                        </Typography>
+                        <Typography className={classes.text}>
+                            There is no denying that I love my career, and I take pride in delivering top-quality, fully tailored services that support my client in reaching new levels of digital success. However, in my free time, you can find me enjoying the outdoors, animals, avidly hiking, and being a proud husband and father. In fact, my family is my biggest inspiration and motivation that pushes me to be the best that I can be.
+                        </Typography>
+                    </div>
+                    <div className={classes.collageContainer}>
+                        <div className={classes.img1}>
+                            <Image
+                                src='/img/profilePicture.jpg'
+                                height={screenHeight * .3}
+                                width={screenWidth * .15}
+                                layout='intrinsic'
+                                // layout='responsive'
+                                objectFit='contain'
+                                // objectPosition='100% 80%'
+                                alt='Personal Photo'
+
+                            />
+                        </div>
+                        <div className={classes.img2}>
+                            <Image
+                                src='/img/profilePicture.jpg'
+                                height={screenHeight * .25}
+                                width={screenWidth * .125}
+                                // layout='intrinsic'
+                                layout='intrinsic'
+                                objectFit='contain'
+                                // objectPosition='40% 100%'
+                                alt='Personal Photo'
+
+                            />
+                        </div>
+                        <div className={classes.img3}>
+                            <Image
+                                src='/img/12721.jpg'
+                                height={screenHeight * .3}
+                                width={screenWidth * .25}
+                                layout='intrinsic'
+                                // layout='responsive'
+                                objectFit='contain'
+                                // objectPosition='100% 80%'
+                                alt='Personal Photo'
+                            />
+                        </div>
+                    </div>
+                </section>
+            }
         </>
     )
 }
---
+++ components/skills/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Skills = () => {
+
+    const useStyles = makeStyles({
+      
+    })
+
+    return(
+        <>
+        <Typography>
+            React
+        </Typography>
+        <Typography>
+            CSS3
+        </Typography>
+        <Typography>
+            MySQL
+        </Typography>
+        <Typography>
+            Node
+        </Typography>
+        <Typography>
+            JavaScript
+        </Typography>
+        <Typography>
+            Redux
+        </Typography>
+        <Typography>
+            Gulp
+        </Typography>
+        <Typography>
+            Git
+        </Typography>
+        <Typography>
+            HTML5
+        </Typography>
+        <Typography>
+            Firebase
+        </Typography>
+        <Typography>
+            Express
+        </Typography>
+        <Typography>
+            Next.js
+        </Typography>
+        <Typography>
+            AWS
+        </Typography>
+        <Typography>
+            React Native
+        </Typography>
+        <Typography>
+            API
+        </Typography>
+        <Typography>
+            MongoDB
+        </Typography>
+        <Typography>
+            SASS
+        </Typography>
+        <Typography>
+            jQuery
+        </Typography>
+        </>
+    )
+}
+
+export default Skills
